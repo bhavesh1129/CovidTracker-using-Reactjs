a8ec457b62b52703c72935846d828506c6306a78
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-06-14 01:52:35 +0530

    Add Code Files

---
+++ src/Table.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Table.css";
+import numeral from "numeral";
+
+function Table({ countries }) {
+  return (
+    <div className="table">
+      {countries.map((country) => (
+        <tr>
+          <td>
+          <img
+              className="table__img"
+              src={country.countryInfo.flag}
+              alt="Flags"
+            />
+          </td>
+          <td>{country.country}</td>
+          <td>
+            <strong>{numeral(country.cases).format("0,0")}</strong>
+          </td>
+        </tr>
+      ))}
+    </div>
+  );
+}
+
+export default Table;
